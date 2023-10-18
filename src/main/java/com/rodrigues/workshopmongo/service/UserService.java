72d986dc2f589fcdda2c46547819ecac6cde4998
yrodr <y.rodr1gues0811@outlook.com>
2023-10-18 17:05:08 -0300

    updating user

--- src/main/java/com/rodrigues/workshopmongo/service/UserService.java
+++ src/main/java/com/rodrigues/workshopmongo/service/UserService.java
@@ -32,8 +32,22 @@ public class UserService {
         findById(id); // chamada apenas para verificar se é um id existente.
         userRepository.deleteById(id);
     }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+
+        updateData(newObj, obj);
+
+        return userRepository.save(newObj);
+    }
+
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
+    public void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 
 }
